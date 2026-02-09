eaaed28cbb22a643a4c5af3c1098975a0d89fdcf
lumos <lumos.wge@gmail.com>
2026-02-09 17:22:19 +0900

    remove region

--- Editor/EditorCreateAssetMenu.cs
+++ Editor/EditorCreateAssetMenu.cs
@@ -4,15 +4,10 @@ namespace LumosLib.Firebase.Editor
 {
     public static class EditorCreateAssetMenu
     {
-        #region >--------------------------------------------------- SO
-
-        
         [MenuItem("Assets/Create/[ LumosLib ]/Prefabs/Manager/Firebase", false, int.MinValue)]
         public static void CreateFirebaseManager()
         {
             LumosLib.Editor.EditorCreateAssetMenu.CreatePrefab<FirebaseManager>();
         }
-        
-        #endregion
     }
 }
